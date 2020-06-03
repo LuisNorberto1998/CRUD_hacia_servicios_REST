a93030a042df95f2780a94d204e9070939712945
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-06-03 03:20:50 -0500

    Cargar productos de Firebase

--- crud_servicios_rest/lib/src/pages/home_page.dart
+++ crud_servicios_rest/lib/src/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/blocs/provider.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_providers.dart';
 
 class HomePage extends StatelessWidget {
+  final productosProvider = new ProductosProvider();
+
   @override
   Widget build(BuildContext context) {
     final bloc = Provider.of(context);
@@ -9,7 +13,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text('Home Page')),
       ),
-      body: Container(),
+      body: _crearListado(),
       floatingActionButton: _crearBoton(context),
     );
   }
@@ -19,6 +23,20 @@ class HomePage extends StatelessWidget {
       onPressed: () => Navigator.pushNamed(context, 'producto'),
       backgroundColor: Colors.deepPurple,
       child: Icon(Icons.add),
+    );
+  }
+
+  Widget _crearListado() {
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+        if (snapshot.hasData) {
+          return Container();
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
--- crud_servicios_rest/lib/src/providers/productos_providers.dart
+++ crud_servicios_rest/lib/src/providers/productos_providers.dart
@@ -18,4 +18,27 @@ class ProductosProvider {
 
     return true;
   }
+
+  //Obtener registros
+  Future<List<ProductoModel>> cargarProductos() async {
+    final url = '$_url/productos.json';
+
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+    final List<ProductoModel> productos = new List();
+
+    if (decodedData == null) return [];
+
+    decodedData.forEach((id, producto) {
+      final prodTemp = ProductoModel.fromJson(producto);
+      prodTemp.id = id;
+
+      productos.add(prodTemp);
+    });
+
+    print(productos);
+
+    return productos;
+  }
 }
